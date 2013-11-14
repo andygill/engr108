8999bec69a66f8650ada7e383cb2baad5ebec1a3
Andy Gill <andygill@ku.edu>
2013-11-13 18:25:57 -0600

    Reworking with login system.

--- content/index.php
+++ content/index.php
@@ -1,4 +1,44 @@
-<!DOCTYPE html>
+<?php
+
+// Holds the user name, *if* login is validated
+$user = $_REQUEST["engr108user"];
+
+function title()
+{
+  print "ENGR 108 Signin Page";
+} 
+
+$hack = "";
+
+
+// First, check for login request.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    if (isset($_REQUEST["logout"])) {
+      setcookie("engr108user","",time() - 3600);
+
+    } elseif (isset($_REQUEST["email"])) {
+
+      // Check _REQUEST["pin"] for hash.
+      // If so, please proceed
+      setcookie("engr108user",$_REQUEST["email"],time() + 3600);
+
+    } else {
+      // Do nothing, just redirect.
+    }
+
+    header( "Location: " . $_SERVER['REQUEST_URI'], 303);
+    die();
+}
+
+// Login depends on engr108user being set.
+
+//cho $_REQUEST["eecs168"];
+
+//  $hack = "<h1>NOT POST</h1>";
+  
+?><!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -8,40 +48,81 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="ico/favicon.png">
 
-    <title>Signin Template for Bootstrap</title>
+    <title><?php title(); ?></title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
 
-    <div class="container">
+   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">ENGR 108</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
 
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Sign into EECS 108</h2>
-        <input type="text" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+<?php if (isset($user)) { ?>
 
-    </div> <!-- /container -->
+          <form class="navbar-form navbar-right" method="POST">
+            <input type="hidden" name="logout">
+            <button type="submit" class="btn btn-primary">Log out</button>
+          </form>
+          <div class="nav navbar-right">
+            <a class="navbar-brand" href="#">Logged in as: <? echo $user; ?></a>
+          </div>
 
+
+<?php } else { ?>
+
+          <form class="navbar-form navbar-right" method="POST">
+            <div class="form-group">
+              <input type="text" placeholder="Email" name="email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="ENGR 108 PIN" name="pin" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form>
+
+<?php } ?>
+
+        </div><!--/.nav-collapse -->
+
+      </div>
+    </div>
+
+ 
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>ENGR 108</h1>
+        <p>Welcome to ENGR 108!</p>
+        <p>Please sign in above with your ENGR 108 PIN.</p>
+      </div>
+    </div>
+
+    <?php echo $hack; ?>
+
+    <?php if (isset($user)) { echo "($user)"; } else { echo "[ NOT SET ]"; } ?>
+
+    <?php echo $_REQUEST["eecs168"]; ?>
+
+    <?php phpinfo(); ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
